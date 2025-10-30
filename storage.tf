a2369e8ed4cdd4e64686ea2d0de182baa6b19ed6
Torgen <118271296+vodnokasanie@users.noreply.github.com>
2025-10-30 18:28:19 +0600

    M

--- storage.tf
+++ storage.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "cmtr_ghjc0xhd_bucket_1761826389" {
+resource "aws_s3_bucket" "cmtr_ghjc0xhd_bucket_1761827232" {
   bucket = var.bucket_name
 
   tags = {
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,1 @@
-bucket_name = "cmtr-ghjc0xhd-bucket-1761826389"
+bucket_name = "cmtr-ghjc0xhd-bucket-1761827232"
