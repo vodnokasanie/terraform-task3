78da98b311fde252f1d4a10cb4c36b96b2266e1e
Torgen <118271296+vodnokasanie@users.noreply.github.com>
2025-10-30 17:58:12 +0600

    M

---
+++ storage.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "cmtr_ghjc0xhd_bucket_1761824170" {
+  bucket = var.bucket_name
+
+  tags = {
+    Project = "cmtr-ghjc0xhd"
+  }
+}
